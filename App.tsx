e2b7194a9da9e1e7675c2d676663b13edcad2c33
TanakaLeo <leonardotanaka0513@gmail.com>
2026-06-06 16:09:46 -0300

    feat: configura fluxo de navegacao em pilha em App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import DashboardScreen from './src/screens/DashboardScreen';
+import TransmitScreen from './src/screens/TransmissionScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="light" />
+      <Stack.Navigator 
+        initialRouteName="Dashboard"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#151D33' },
+          headerTintColor: '#FFF',
+          headerTitleStyle: { fontWeight: 'bold' },
+        }}
+      >
+        <Stack.Screen 
+          name="Dashboard" 
+          component={DashboardScreen} 
+          options={{ title: 'Painel de Controle' }} 
+        />
+        <Stack.Screen 
+          name="Transmitir" 
+          component={TransmitScreen} 
+          options={{ title: 'Painel de Transmissão' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
